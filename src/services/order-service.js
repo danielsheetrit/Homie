7c00b7b5a00dab90d6e4c8f02c39c3d0f0979567
Din <dinos4620@gmail.com>
2021-06-07 15:04:03 +0200

    dino

--- src/services/order-service.js
+++ src/services/order-service.js
@@ -8,7 +8,7 @@ export const orderService = {
     remove,
     add,
     update,
-    calcDays
+    
 }
 
 async function query(user = { _id: null }) {
@@ -20,7 +20,6 @@ async function query(user = { _id: null }) {
 }
 
 async function add(trip, stay, loggedInUser) {
-<<<<<<< HEAD
     try {
         const order = {
             // _id: utilService.makeId(),
@@ -47,32 +46,6 @@ async function add(trip, stay, loggedInUser) {
         return await httpService.post(`order`, order)
     } catch (err) {
         console.log('cant add new order', err)
-=======
-    const days = calcDays(trip.startDate, trip.endDate)
-    const totalPrice = stay.price * days + 10
-    const order = {
-        // _id: utilService.makeId(),
-        createdAt: Date.now(),
-        buyer: {
-            _id: loggedInUser._id,
-            fullName: loggedInUser.fullname
-        },
-        totalPrice,
-        city: stay.loc.city,
-        startDate: trip.startDate.format('YYYY-MM-DD'),
-        endDate: trip.endDate.format('YYYY-MM-DD'),
-        guests: {
-            adults: trip.adults,
-            children: trip.children,
-        },
-        stay: {
-            _id: stay._id,
-            name: stay.name,
-            price: stay.price
-        },
-        host: stay.host,
-        status: 'pending',
->>>>>>> 4c08d29072170058f5637a99f20a8f663a56df54
     }
 }
 
@@ -85,22 +58,10 @@ function remove(orderId) {
 }
 
 async function update(order) {
-<<<<<<< HEAD
     try {
         return await httpService.put(`order/${order._id}`, order)
     } catch (err) {
         console.log('cant update order', err)
     }
 }
-=======
-    return await httpService.put(`order/${order._id}`, order)
-
-}
-
-function calcDays(startDate, endDate) {
-    const firstDay = moment(startDate.format('YYYY-MM-DD'))
-    const lastDay = moment(endDate.format('YYYY-MM-DD'))
-    const days = lastDay.diff(firstDay, 'days') + 1
-    return days;
-}
->>>>>>> 4c08d29072170058f5637a99f20a8f663a56df54
+ 
